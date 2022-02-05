b0a193341c0a1860418f4fdf895d8e880d8173eb
playground <connie.s.barrantes@gmail.com>
2022-02-05 16:15:01 -0500

    moved html files back to public folder

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,12 +3,12 @@ const router = require('express').Router();
 
 // call for home page
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../index.html'));
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 // call for notes.html
 router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../notes.html'));
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
 module.exports = router;
