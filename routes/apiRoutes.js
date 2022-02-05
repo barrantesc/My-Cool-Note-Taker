f0f20c9fb353157dc246611be389e7494b93c718
playground <connie.s.barrantes@gmail.com>
2022-02-05 15:15:49 -0500

    fixed code to post and delte notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,27 +1,44 @@
 const router = require('express').Router();
-const { notes } = require("../db/db.json");
-const { createNewNote, deleteNote } = require("../lib/notes");
+const fs = require('fs');
+// const { notes } = require("../db/db.json");
+// const { createNewNote, deleteNote } = require("../lib/notes");
+const path = require('path');
+const uniqueID = require('uniqid');
 
 // get
 router.get('/notes', (req, res) => {
-    if (notes) {
-        res.send(notes);
-    } else {
-        res.json("goodbye")
-    }
-});
+    const db = JSON.parse(fs.readFileSync("./db/db.json"));
+    res.send(db)
+}); 
 
 // post
 router.post('/notes', (req, res) => {
-    req.body.id = notes.length.toString();
 
-    const newNote = createNewNote(req.body, notes);
-    res.json(newNote);
+    let noteId = uniqueID();
+    const db = JSON.parse(fs.readFileSync("./db/db.json"));
+    res.json(db)
+   console.log(noteId);
+
+    const newNote = {
+        title: req.body.title, text: req.body.text, id: noteId,
+    };
+    db.push(newNote);
+    fs.writeFileSync("./db/db.json", JSON.stringify(db));
 });
 
 //delete
 router.delete('/notes/:id', (req, res) => {
-    res.send(deleteNote(req.params.id, notes));
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8")); // reads db.json
+    let noteID = savedNotes.filter(x=>x.id!=req.params.id) // returns route with all notes EXCEPT the ID we are deleting
+    console.log("NOTE ID", noteID)
+    console.log("REQ.PARAMS.ID", req.params.id)
+    
+    fs.writeFileSync("./db/db.json", JSON.stringify(noteID), (err) => {
+        if (err) throw err;
+        console.log("error");
+    });
+    console.log("Your note has been deleted");
+    return res.json(savedNotes);
 });
 
 module.exports = router;
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,13 +1,14 @@
 const path = require('path');
 const router = require('express').Router();
 
-
+// call for home page
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
+// call for notes.html
 router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
 module.exports = router;
